228b768e15505c0976abf1a7d00505523f003e8e
TomiGorog <tamas.peter.gorog@gmail.com>
2022-08-19 18:31:18 +0800

    rnnr project receiver isnt finished

--- src/RnnRProject.js
+++ src/RnnRProject.js
@@ -9,16 +9,49 @@ function RnnRProject() {
       <p>During the projects' ideation phase we talked about potentional user groups,
          their motivations and the actions users have to take to react their goals:</p>
       <ul>
-        <h4>Sender</h4>
+        <h4 className={ProjectStyle.SubTitle}>Sender</h4>
         <li>Motivations: Cost effective delivery, quick delivery, customer satisfaction and on time delivery with control over the packages</li>
 
-        <ol>Needed to react the goals:
+        <ol>
+          <h5>Needed to react the goals:</h5>
         <li> registration / login, </li>
         <li>upload package parameters, </li>
         <li>Give package to runner</li>
         <li>Package tracking</li>
         <li>Searching sent packages</li>
         <li>Following packages when being a receiver</li>
+        <li>Being able to modify personal data</li>
+        </ol>
+      </ul>
+      <ul>
+        <h4 className={ProjectStyle.SubTitle}>Runner</h4>
+        <li>Motivations: Financial goals, good review more job, instant job search</li>
+
+        <ol>
+          <h5>Needed to react the goals:</h5>
+        <li> registration / login, </li>
+        <li>Search for available packages </li>
+        <li>Apply for the choosen package</li>
+        <li>Pickup the package</li>
+        <li>Deliver the package with GPS tracking and QR-based delivery</li>
+        <li>Quickly be able to apply for other packages</li>
+        <li>Track delivered packages, income and ratings</li>
+        <li>Being able to modify personal data</li>
+        </ol>
+      </ul>
+      <ul>
+        <h4 className={ProjectStyle.SubTitle}>Receiver</h4>
+        <li>Motivations: Intact package arriving on time, package tracking, reviewing delivery performance, cost effective delivery</li>
+
+        <ol>
+          <h5>Needed to react the goals:</h5>
+        <li> registration / login, </li>
+        <li>Search for available packages </li>
+        <li>Apply for the choosen package</li>
+        <li>Pickup the package</li>
+        <li>Deliver the package with GPS tracking and QR-based delivery</li>
+        <li>Quickly be able to apply for other packages</li>
+        <li>Track delivered packages, income and ratings</li>
         <li>Being able to modify personal data</li>
         </ol>
       </ul>
