951dfe2a65326b43fa1efe94dcd6c6e3ef9ca2d8
TomiGorog <tamas.peter.gorog@gmail.com>
2022-08-21 16:45:47 +0800

    arrows need  navigation

--- src/Pagedown.js
+++ src/Pagedown.js
@@ -1,10 +1,20 @@
 import React from 'react'
 
+import { AiOutlineArrowLeft, AiOutlineArrowUp } from 'react-icons/ai';
+
+
 function Pagedown() {
   return (
-    <div>
+    <div className='pagebottomcontainer'>
+      <div className='pagebottominnerbox'>
+      <AiOutlineArrowLeft  size={40}/>
+      <span className='arrowtext'>Back to projects</span>
+      </div>
+      <div className='pagebottominnerbox'>
+      <AiOutlineArrowUp size={40} />
+      <span className='arrowtext'>Top of the page</span>
+      </div>
 
-<FontAwesomeIcon icon="fa-solid fa-arrow-left" />
     </div>
   )
 }
