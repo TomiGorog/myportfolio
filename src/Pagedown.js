5d312d0bcaadf48a2d20a7553ba54a492e9f90ac
TomiGorog <tamas.peter.gorog@gmail.com>
2022-08-22 12:41:46 +0800

    scroll component not working yet

--- src/Pagedown.js
+++ src/Pagedown.js
@@ -1,19 +1,33 @@
 import React from 'react'
 
 import { AiOutlineArrowLeft, AiOutlineArrowUp } from 'react-icons/ai';
+import {useNavigate} from "react-router-dom"
 
+function Pagedown(props) {
+  let navigate = useNavigate()
+  
 
-function Pagedown() {
   return (
-    <div className='pagebottomcontainer'>
-      <div className='pagebottominnerbox'>
+    <div  className='pagebottomcontainer'>
+      <div 
+      onClick={() => {
+        navigate("/projects")
+      }}
+      
+      className='pagebottominnerbox'>
       <AiOutlineArrowLeft  size={40}/>
       <span className='arrowtext'>Back to projects</span>
       </div>
-      <div className='pagebottominnerbox'>
+      {/* <div 
+      onClick={() => {props.scrollUp()
+      props.trialFunct()
+      }
+    
+    }
+      className='pagebottominnerbox'>
       <AiOutlineArrowUp size={40} />
       <span className='arrowtext'>Top of the page</span>
-      </div>
+      </div> */}
 
     </div>
   )
