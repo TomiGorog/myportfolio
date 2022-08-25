97c9942f403689d5b2c4e95ba1d6e6d1c195aeb4
TomiGorog <tamas.peter.gorog@gmail.com>
2022-08-25 20:28:44 +0800

    accent color on active navlink

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,20 +1,22 @@
 import React from 'react'
 
-import { Divide as Hamburger } from 'hamburger-react'
 import NavStyle from './CSS-modules/NavigationStyles.module.css'
-import ReactSwitch from 'react-switch';
-
 import { ThemeContext } from './App'
 import { Link, useNavigate } from 'react-router-dom'
+
 function Navigation() {
-    const [isOpen, setOpen] = React.useState(false)
-    let colorContext = React.useContext(ThemeContext)
+    // const [isOpen, setOpen] = React.useState(false)
+    // let colorContext = React.useContext(ThemeContext)
+
+    const [activeTab, setActiveTab] = React.useState(undefined)
+
     let navigate = useNavigate()
     return (
         <div className='navigation'>
                 <div 
                 onClick={() => {
                    navigate("/") 
+                   setActiveTab(undefined)
                 }}
                 className='logo'>TGDev</div>
             {/* <div className={NavStyle.LogoSwitch}> */}
@@ -24,9 +26,25 @@ function Navigation() {
                 {/* </div> */} 
             {/* </div> */}
             <div className={NavStyle.TabsMenu}>
-                <Link to="about">About</Link>
-                <Link to="projects">Projects</Link>
-                <Link to="contact">Contact</Link>
+                <Link onClick={() => {
+                    setActiveTab("about")
+                }} 
+                to="about"
+                id={activeTab == "about" ? "activeTab" : ""}
+                >About</Link>
+                <Link onClick={() => {
+                    setActiveTab("projects")
+                }} 
+                to="projects"
+                id={activeTab == "projects" ? "activeTab" : ""}
+                >Projects</Link>
+                <Link onClick={() => {
+                    setActiveTab("contact")
+                }} 
+                to="contact"
+                id={activeTab == "contact" ? "activeTab" : ""}
+                >Contact</Link>
+                
                 <a href="TamasGorog_Frontend_Developer.pdf" target="_blank" >Resume</a>
             </div>
 
